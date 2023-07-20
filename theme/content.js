ad8ead6af91c402f43f4d3c6aa27500590ca143f
haohao <haohao.yu@qq.com>
2023-07-20 20:05:47 +0800

    init

---
+++ theme/content.js
@@ -0,0 +1,60 @@
+export default ({ children, meta = {} }) => (
+  <div className={meta.lang ? 'lang-' + meta.lang : ''}>
+    {children}
+    <style jsx>{`
+      div {
+        margin: 2rem 0 5rem;
+        font-family: 'Helvetica', sans-serif;
+      }
+      div :global(p) {
+        margin-top: 1.5rem;
+        margin-bottom: 0;
+      }
+      div :global(img) {
+        max-width: 100%;
+      }
+
+      div.lang-zh :global(p),
+      div.lang-zh :global(li) {
+        text-align: justify;
+        text-justify: auto;
+      }
+      div.lang-zh :global(em) {
+        text-emphasis: filled dot;
+        text-emphasis-position: under;
+        font-style: inherit;
+      } 
+
+      div :global(li > p) {
+        margin-top: 0;
+      }
+
+      div :global(li) {
+        line-height: 1.7;
+      }
+      
+      // :global(:target) {
+      //   background: #fffa9e;
+      // }
+      // :global(ul) {
+      //   list-style-type: none;
+      //   padding: 0;
+      // }
+      // :global(li) {
+      //   padding-left: 1.1875em;
+      //   position: relative;
+      //   margin-bottom: .25em;
+      // }
+      // :global(li):before {
+      //   content: "•";
+      //   position: absolute;
+      //   left: .2em;
+      // }
+
+      :global(a) {
+        color: black; /* 设置超链接的颜色为黑色 */
+        text-decoration: none; /* 取消超链接的下划线 */
+      }
+    `}</style>
+  </div>
+)
